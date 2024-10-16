c5a545166c8017aaf1e59d9d8c4623babe7a2e17
20chan <2@0chan.dev>
2024-10-16 13:24:05 +0900

    impl third place match

--- src/components/Bracket_v2.tsx
+++ src/components/Bracket_v2.tsx
@@ -1,9 +1,10 @@
 import { TournamentV2Model } from '@/lib/db/tournament_v2';
 import { Bracket, BracketGenerator } from 'react-tournament-bracket';
-import { createGame } from '@/lib/game_v2';
+import { createGame, createThirdPlaceGame } from '@/lib/game_v2';
 import { GameComponentProps } from 'react-tournament-bracket/lib/components/Bracket';
 import { GameExt, SideInfoExt } from 'react-tournament-bracket/lib/components/model';
 import { RectClipped as RectClipped0 } from 'react-tournament-bracket/lib/components/Clipped';
+import winningPathLength from 'react-tournament-bracket/lib/util/winningPathLength';
 import classNames from 'classnames';
 import React from 'react';
 import Image from 'next/image';
@@ -11,12 +12,15 @@ import { useTournamentContext } from './TournamentProvider';
 import { mapInfos } from '@/lib/sl/map';
 
 const RectClipped = RectClipped0 as any;
+const Bracket0 = Bracket as any;
 
 interface SLGameComponentProps extends GameComponentProps {
   customViewBox?: string;
+  bottomText?: string;
+  isThirdMatch?: boolean;
 }
 
-export function SLGame({ game: game0, x, y, customViewBox }: SLGameComponentProps) {
+export function SLGame({ game: game0, x, y, customViewBox, bottomText, isThirdMatch }: SLGameComponentProps) {
   const game = game0 as GameExt;
 
   const { mapHandler, winHandler } = useTournamentContext();
@@ -45,13 +49,13 @@ export function SLGame({ game: game0, x, y, customViewBox }: SLGameComponentProp
     return (
       <g>
         <RectClipped x={x} y={y} width={315} height={25}>
-          <text x={x + 14} y={y + 17} textAnchor='middle' className={classNames('font-sl fill-white/80', lost && 'fill-half-white/30')}>
+          <text x={x + 14} y={y + 17} textAnchor='middle' className={classNames('font-sl', lost ? 'fill-half-white/30' : 'fill-white/80')}>
             {side.team?.id}
           </text>
-          <text x={x + 28} y={y + 17} className={classNames('font-sl fill-yellow-500', lost && 'fill-yellow-500/30')}>
+          <text x={x + 28} y={y + 17} className={classNames('font-sl', lost ? 'fill-yellow-500/30' : 'fill-yellow-500')}>
             {side.team?.clan}
           </text>
-          <text x={x + 78} y={y + 17} wordSpacing={4} className={classNames('font-noto fill-white/80 text-sm', lost && 'fill-half-white/30')}>
+          <text x={x + 78} y={y + 17} wordSpacing={4} className={classNames('font-noto text-sm', lost ? 'fill-half-white/30' : 'fill-white/80')}>
             {side.team?.members?.join(' ')}
           </text>
         </RectClipped>
@@ -61,61 +65,62 @@ export function SLGame({ game: game0, x, y, customViewBox }: SLGameComponentProp
 
   const width = 350;
   return (
-    <svg width={width} height='82' viewBox={customViewBox ?? '0 0 350 82'} x={x} y={y}>
-      <rect x='0' y='12' width={width} height='25' fill={topWon ? '#224076ff' : '#22407655'} />
-      <rect x='0' y='37' width={width} height='25' fill={bottomWon ? '#602A35ff' : '#602A3555'} />
-      <rect x='0' y='62' width={width} height='25' fill='#00000050' />
+    <>
+      <svg width={width} height='82' viewBox={customViewBox ?? '0 0 350 82'} x={x} y={y}>
+        <rect x='0' y='12' width={width} height='25' fill={topWon ? '#224076ff' : '#22407655'} />
+        <rect x='0' y='37' width={width} height='25' fill={bottomWon ? '#602A35ff' : '#602A3555'} />
+        <rect x='0' y='62' width={width} height='25' fill='#00000050' />
 
-      <rect x={width - 40} y='12' width='40' height='25' fill='#10131c'
-        onPointerEnter={() => setTopWinHover(true)}
-        onPointerLeave={() => setTopWinHover(false)}
-        onClick={() => top.team && winHandler(match.id, parseInt(top.team.id), false)}
-        className='cursor-pointer'
-      />
-      <rect x={width - 40} y='37' width='40' height='25' fill='#10131c'
-        onPointerEnter={() => setBottomWinHover(true)}
-        onPointerLeave={() => setBottomWinHover(false)}
-        onClick={() => bottom.team && winHandler(match.id, parseInt(bottom.team.id), false)}
-        className='cursor-pointer'
-      />
+        <rect x={width - 40} y='12' width='40' height='25' fill='#10131c'
+          onPointerEnter={() => setTopWinHover(true)}
+          onPointerLeave={() => setTopWinHover(false)}
+          onClick={() => top.team && winHandler(match.id, parseInt(top.team.id), isThirdMatch ?? false)}
+          className='cursor-pointer'
+        />
+        <rect x={width - 40} y='37' width='40' height='25' fill='#10131c'
+          onPointerEnter={() => setBottomWinHover(true)}
+          onPointerLeave={() => setBottomWinHover(false)}
+          onClick={() => bottom.team && winHandler(match.id, parseInt(bottom.team.id), isThirdMatch ?? false)}
+          className='cursor-pointer'
+        />
 
-      <text x={width - 28} y='29' className={classNames('font-sl pointer-events-none', !(topWon && !bottomWon) && !topWinHover && 'fill-white/80', (topWon && !bottomWon) && !topWinHover && 'fill-green-400', topWinHover && 'fill-half-yellow')}>
-        {topWinHover ? '승?' : (topWon && !bottomWon) ? '승' : (!topWon && bottomWon) ? '패' : ''}
-      </text>
+        <text x={width - 28} y='29' className={classNames('pointer-events-none', !(topWon && !bottomWon) && !topWinHover && 'fill-white/80', (topWon && !bottomWon) && !topWinHover && 'fill-green-400', topWinHover && 'fill-half-yellow')}>
+          {topWinHover ? '승?' : (topWon && !bottomWon) ? '승' : (!topWon && bottomWon) ? '패' : ''}
+        </text>
 
-      <text x={width - 28} y='54' className={classNames('font-sl pointer-events-none', !(bottomWon && !topWon) && !bottomWinHover && 'fill-white/80', (bottomWon && !topWon) && !bottomWinHover && 'fill-green-400', bottomWinHover && 'fill-half-yellow')}>
-        {bottomWinHover ? '승?' : (bottomWon && !topWon) ? '승' : (!bottomWon && topWon) ? '패' : ''}
-      </text>
+        <text x={width - 28} y='54' className={classNames('pointer-events-none', !(bottomWon && !topWon) && !bottomWinHover && 'fill-white/80', (bottomWon && !topWon) && !bottomWinHover && 'fill-green-400', bottomWinHover && 'fill-half-yellow')}>
+          {bottomWinHover ? '승?' : (bottomWon && !topWon) ? '승' : (!bottomWon && topWon) ? '패' : ''}
+        </text>
 
-      {
-        top ? (
-          <Side x={0} y={12} side={top} lost={!topWon && bottomWon} />
-        ) : null
-      }
+        {
+          top ? (
+            <Side x={0} y={12} side={top} lost={!topWon && bottomWon} />
+          ) : null
+        }
 
-      {
-        bottom ? (
-          <Side x={0} y={37} side={bottom} lost={!bottomWon && topWon} />
-        ) : null
-      }
+        {
+          bottom ? (
+            <Side x={0} y={37} side={bottom} lost={!bottomWon && topWon} />
+          ) : null
+        }
 
-      {
-        maps.length === 0 ? (
-          <>
-            <text x={width / 4} y='76' textAnchor='middle' width={width / 2} height='20'
-              className='font-noto text-xs fill-white hover:fill-half-yellow cursor-pointer'
-              onClick={() => mapHandler(match.id, 3, false)}
-            >
-              3라운드
-            </text>
-            <text x={width * 3 / 4} y='76' textAnchor='middle' width={width / 2} height='20'
-              className='font-noto text-xs fill-white hover:fill-half-yellow cursor-pointer'
-              onClick={() => mapHandler(match.id, 5, false)}
-            >
-              5라운드
-            </text>
-          </>
-        ) : (
+        {
+          maps.length === 0 ? (
+            <>
+              <text x={width / 4} y='76' textAnchor='middle' width={width / 2} height='20'
+                className='font-noto text-xs fill-white hover:fill-half-yellow cursor-pointer'
+                onClick={() => mapHandler(match.id, 3, isThirdMatch ?? false)}
+              >
+                3라운드
+              </text>
+              <text x={width * 3 / 4} y='76' textAnchor='middle' width={width / 2} height='20'
+                className='font-noto text-xs fill-white hover:fill-half-yellow cursor-pointer'
+                onClick={() => mapHandler(match.id, 5, isThirdMatch ?? false)}
+              >
+                5라운드
+              </text>
+            </>
+          ) : (
             <foreignObject x='0' y='62' width={width} height='20'>
               <div className='relative flex-1'>
                 <div className='absolute inset-0 flex flex-row justify-stretch items-stretch overflow-clip'>
@@ -125,24 +130,31 @@ export function SLGame({ game: game0, x, y, customViewBox }: SLGameComponentProp
                     </div>
                   ))}
                 </div>
-                <span className='flex flex-row justify-stretch'>
+                  <span className='flex flex-row justify-stretch'>
                   {maps.map(x => mapInfos[x - 1].name).map(x => (
                     <div key={x} className='flex-1 text-center'>
-                      <span className='font-noto text-white text-sm'>{x}</span>
+                      <span className='font-noto text-white text-xs align-top'>{x}</span>
                     </div>
                   ))}
                 </span>
               </div>
             </foreignObject>
-        )
-      }
+          )
+        }
 
-      {
-        (topWon || bottomWon) && !isLastMatch && (
-          <rect x='0' y='12' width={width} height='75' className='fill-black/50 z-30 pointer-events-none' />
-        )
-      }
-    </svg>
+        {
+          (topWon || bottomWon) && !isLastMatch && (
+            <rect x='0' y='12' width={width} height='75' className='fill-black/50 z-30 pointer-events-none' />
+          )
+        }
+      </svg>
+
+      <foreignObject x={x} y={y + 81} width={width} height='20'>
+        <div className='w-[350px] text-center text-sm text-white/60 mt-1'>
+          {isLastMatch && (bottomText ?? '결승')}
+        </div>
+      </foreignObject>
+    </>
   )
 }
 
@@ -151,11 +163,38 @@ export function BracketV2({
 }: {
   model: TournamentV2Model;
   }) {
+  const game = createGame(model);
+  const numRounds = winningPathLength(game);
+
+  const gameDimensions = { width: 350, height: 82 };
+  const svgDimensions = {
+    width: numRounds * (gameDimensions.width + 24) + 40,
+    height: gameDimensions.height * Math.pow(2, numRounds - 1) + 40,
+  }
+
   return (
     <div className='relative w-fit'>
-      <div>
-        <Bracket game={createGame(model)} GameComponent={SLGame} gameDimensions={{ width: 350, height: 82 }} />
-      </div>
+      <Bracket game={game} GameComponent={SLGame} gameDimensions={gameDimensions} />
+
+      {
+        model.options.thirdPlaceEnabled && (
+          <div style={{
+            position: 'absolute',
+            top: svgDimensions.height / 2 + gameDimensions.height / 2 + 40,
+            left: svgDimensions.width - 350 - 20,
+          }}>
+            <SLGame
+              game={createThirdPlaceGame(model)}
+              customViewBox='0 0 350 82'
+              x={0}
+              y={0}
+              homeOnTop={true}
+              bottomText='3/4위 결정전'
+              isThirdMatch
+            />
+          </div>
+        )
+      }
     </div>
   );
 }
--- src/lib/game_v2.ts
+++ src/lib/game_v2.ts
@@ -1,5 +1,6 @@
 import { Game, GameExt } from 'react-tournament-bracket/lib/components/model';
-import { TournamentV2Model } from './db/tournament_v2';
+import { TournamentV2MatchModel, TournamentV2Model } from './db/tournament_v2';
+import { getThirdMatch } from './tournamentHelper_v2';
 
 export function createMatchGame(model: TournamentV2Model, matchId: number): GameExt {
   const match = model.matches.find(x => x.id === matchId)!;
@@ -85,4 +86,66 @@ export function createGame(model: TournamentV2Model): Game {
   const finalMatch = model.matches.find(x => x.shape.nextMatchId === null)!;
 
   return createMatchGame(model, finalMatch.id);
+}
+
+export function createThirdPlaceGame(model: TournamentV2Model): GameExt {
+  const thirdMatchTeamIds = getThirdMatch(model);
+
+  const {
+    thirdPlaceMaps,
+    thirdPlaceWinId,
+  } = model.options;
+
+  const [team1, team2] = thirdMatchTeamIds.map(x => x !== null ? model.teams.find(team => team.id === x) ?? null : null);
+
+  const match: TournamentV2MatchModel = {
+    id: 999,
+    shape: {
+      nextMatchId: null,
+      prevMatch1Id: null,
+      prevMatch2Id: null,
+      match1TeamId: null,
+      match2TeamId: null,
+      depth: 0,
+      index: 0,
+      mapCount: 5,
+      nextMatchIndex: null,
+    },
+    win: thirdPlaceWinId === null ? null : thirdPlaceWinId === thirdMatchTeamIds[0] ? 0 : 1,
+    winner: thirdPlaceWinId,
+    loser: thirdPlaceWinId === null ? null : thirdPlaceWinId === thirdMatchTeamIds[0] ? thirdMatchTeamIds[1] : thirdMatchTeamIds[0],
+    maps: thirdPlaceMaps,
+  }
+
+  return {
+    id: '999',
+    name: '',
+    scheduled: 0,
+    sides: {
+      home: {
+        team: team1 ? {
+          id: team1.id.toString(),
+          name: team1.name,
+          clan: team1.clan,
+          members: team1.members,
+        } : undefined,
+        score: {
+          score: team1?.id === model.options.thirdPlaceWinId ? 1 : 0,
+        },
+      },
+      visitor: {
+        team: team2 ? {
+          id: team2.id.toString(),
+          name: team2.name,
+          clan: team2.clan,
+          members: team2.members,
+        } : undefined,
+        score: {
+          score: team2?.id === model.options.thirdPlaceWinId ? 1 : 0,
+        },
+      },
+    },
+    maps: model.options.thirdPlaceMaps,
+    match,
+  };
 }
